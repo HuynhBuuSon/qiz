412cd6ba03ab210f8cb9b0943736bd1550410ed2
Son Huynh <buuson@hotmail.com>
2025-12-10 16:32:27 +0700

    Remove src1

--- src1/src/app/api/swagger.json/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { swaggerSpec } from '@/lib/swagger';
-
-export function GET(request: NextRequest) {
-  return NextResponse.json(swaggerSpec);
-}
